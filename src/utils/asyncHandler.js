7d49fb0cbf1a98fe93fc6be427e4f9497b27c5b2
Rajdeep <rajdeepmukherjee2002@gmail.com>
2023-11-11 21:13:57 +0530

    utility configuration

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler1 = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+const asyncHandler2 = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) =>
+            next(err)
+        );
+    };
+};
+
+export { asyncHandler1, asyncHandler2 };
